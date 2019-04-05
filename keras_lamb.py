f2305fdc5ad6920218f4770540ddbe1479364a80
Somshubra Majumdar <titu1994@gmail.com>
2019-04-04 19:47:41 -0500

    Update docs

--- keras_lamb.py
+++ keras_lamb.py
@@ -12,11 +12,9 @@ class LAMB(Optimizer):
         lr: float >= 0. Learning rate.
         beta_1: float, 0 < beta < 1. Generally close to 1.
         beta_2: float, 0 < beta < 1. Generally close to 1.
-        epsilon: float >= 0. Fuzz factor. If `None`, defaults to `K.epsilon()`.
+        epsilon: float >= 0. Fuzz factor. If `None`, defaults to 1e-6.
+        weight_decay: float >= 0. Weight decay regularization.
         decay: float >= 0. Learning rate decay over each update.
-        amsgrad: boolean. Whether to apply the AMSGrad variant of this
-            algorithm from the paper "On the Convergence of Adam and
-            Beyond".
 
     # References
         - [Reducing BERT Pre-Training Time from 3 Days to 76 Minutes]
